90a5b8da264e9a6d009830af62ada83f14c066b1
Liangzhijian1996 <liangzhijian1996@163.com>
2020-03-20 16:54:42 +0800

    subclass savedmodel

--- mlp_subclass.py
+++ mlp_subclass.py
@@ -1,92 +1,65 @@
-import datetime
-import os
-import tensorflow as tf
 from tensorflow import keras
-import time
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
 
 
-def dataset():
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-    x_train, x_test = x_train / 255.0, x_test / 255.0
-    # Add a channels dimension
-    x_train = x_train[..., tf.newaxis]
-    x_test = x_test[..., tf.newaxis]
-
-    train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(10000).batch(8)
-    test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(8)
-    return train_dataset, test_dataset
-
-
-class MyModel(tf.keras.Model):
+class my_model(tf.keras.Model):
     def __init__(self):
-        super(MyModel, self).__init__()
-        self.conv1 = keras.layers.Conv2D(32, 3, activation='relu')
-        self.flatten = keras.layers.Flatten()
-        self.dense1 = keras.layers.Dense(128, activation='relu')
-        self.dense2 = keras.layers.Dense(10, activation='softmax')
+        super(my_model, self).__init__()
+        self.block_1 = keras.layers.Dense(64, activation='relu', name='dense_1')
+        self.block_2 = keras.layers.Dense(64, activation='relu', name='dense_2')
+        self.block_3 = keras.layers.Dense(10, name='predictions')
 
-    @tf.function
-    def call(self, x):
-        x = self.conv1(x)
-        x = self.flatten(x)
-        x = self.dense1(x)
-        return self.dense2(x)
+    def call(self, inputs):
+        x = self.block_1(inputs)
+        x = self.block_2(x)
+        outputs = self.block_3(x)
+        return outputs
 
 
-model = MyModel()
-train_ds, test_ds = dataset()
-loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+optimizer = keras.optimizers.SGD(learning_rate=1e-3)
+loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
-train_loss = tf.keras.metrics.Mean(name='train_loss')
-train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+# 数据处理部分
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+x_train = x_train.reshape(60000, 784).astype('float32') / 255
+y_train = y_train.astype('float32')
 
-test_loss = tf.keras.metrics.Mean(name='test_loss')
-test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
-
-
-# @tf.function
-def train_step(inputs, targets):
-    with tf.GradientTape() as tape:
-        predictions = model(inputs)
-        loss = loss_object(targets, predictions)
-    grads = tape.gradient(loss, model.variables)
-    optimizer.apply_gradients(grads_and_vars=zip(grads, model.variables))
-    loss = train_loss(loss)
-    accuracy = train_accuracy(targets, predictions)
-    return loss, accuracy
+train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+train_dataset = train_dataset.batch(64)
 
 
-@tf.function
-def test_step(inputs, targets):
-    predictions = model(inputs)
-    t_loss = loss_object(targets, predictions)
-    test_loss(t_loss)
-    test_accuracy(targets, predictions)
+def train():
+    # 借用 compile+fit 指定模型各层形状
+    model = my_model()
+    # model.compile(optimizer=keras.optimizers.SGD(learning_rate=1e-3),
+    #               loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True))
+    # model.fit(x_train, y_train, batch_size=64, epochs=1)
+
+    # 进行自定义训练
+    for epoch in range(1):
+        for step, (x_batch_train, y_batch_train) in enumerate(train_dataset):
+            with tf.GradientTape() as tape:
+                logits = model(x_batch_train, training=True)
+                loss_value = loss_fn(y_batch_train, logits)
+            grads = tape.gradient(loss_value, model.trainable_weights)
+            optimizer.apply_gradients(zip(grads, model.trainable_weights))
+            if step % 200 == 0:
+                print('Training loss at step %s: %s' % (step, float(loss_value)))
+    # model.save('saved/my_model_2', save_format='tf')
+    model.save_weights('saved/my_weights', save_format='tf')
 
 
-if __name__ == '__main__':
-    s_time = time.time()
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
-    train_log_dir = 'logs/train-' + current_time
-    train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+def test():
+    new_model = my_model()
+    new_model.load_weights('saved/my_weights')
 
-    epoch = 1
-    for epoch_index in range(epoch):
-        batch_index = int()
-        for (images, labels) in train_ds.take(3):
-            tf.summary.trace_on(graph=True, profiler=True)
-            loss, accuracy = train_step(images, labels)
-            with train_summary_writer.as_default():
-                tf.summary.trace_export(name="my_func_trace", step=0, profiler_outdir=train_log_dir)
+    # new_model = keras.models.load_model('saved/my_model_2')
 
-            with train_summary_writer.as_default():
-                tf.summary.scalar('loss', loss, step=batch_index)
-                tf.summary.scalar('accuracy', accuracy, step=batch_index)
-            batch_index +=1
+    for step, (x_batch_train, y_batch_train) in enumerate(train_dataset.take(1)):
+        y_prep = new_model(x_batch_train, training=True)
+        loss = loss_fn(y_batch_train, y_prep)
+        print(loss.numpy())
 
-            template = 'batch {}, Loss: {:.4f}, Accuracy: {:.4f}'
-            print(template.format(batch_index, loss, accuracy))
-    print('total time:', time.time() - s_time)
+
+# train()
+# test()
